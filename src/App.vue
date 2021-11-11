0321733b8ad50173bed37d7b35a686dbe16d1622
BonnyWang <bonny.y.gardy@gmail.com>
2021-11-10 22:26:44 -0500

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <carousel-3d  :height="slideHeight" :width="500" :controls-visible="true" :controls-prev-html="'&#10092; '" :controls-next-html="'&#10093;'" 
+               :controls-width="30" :controls-height="60" :clickable="false">
+    <slide v-for="(slide, i) in slides" :key="i" :index="i">
+      <img :src="images[i]"/>
+    </slide>
+  </carousel-3d>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {Carousel3d, Slide } from 'vue-carousel-3d';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Carousel3d,
+    Slide
+  },
+  data: function() {
+    return {
+      slides: 5,
+      slideHeight: 500,
+      images:["hi.png","why.jpg"]
+    }
+  },
+  methods:{
+    getViewHeight(){
+      this.slideHeight = window.innerHeight-100;
+    }
+  },
+  mounted(){
+    this.getViewHeight()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: black;
 }
 </style>
